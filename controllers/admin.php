1ada99301a09903a51fbfa467ec32c878a1add58
Rephyroth <57422543+Rephyroth@users.noreply.github.com>
2021-05-25 15:10:06 -0500

    last commit Fernando

--- controllers/admin.php
+++ controllers/admin.php
@@ -2,15 +2,103 @@
 class Admin extends Controller{
     function __construct(){
         parent::__construct();
-        //echo "<p>Nuevo Controlador MAin</p>";
+        $this ->view -> casas = [];
     }
      function render(){
+        //trae el metodo get con el model 
+        $casa = $this ->model-> get();
+        //manda los datos del model a la vista 
+        $this ->view -> casas = $casa;
+        
         //reenderiza la vista especifica
         $this -> view ->render('admin/index');
+        
     }
-
     function saludo(){
         echo "<p>Nuevo saludo</p>";
     }
+
+    function verCasa($param = null){
+        //var_dump($param);
+        //metemos el parametro que se pide en una variable
+        $casa_id = $param[0];
+        //manda el parametro con el model a la funcion getById
+        $casa = $this -> model -> getById($casa_id);
+        //crear una sesion para seguridad de datos
+        session_start();
+        //creamos una variable de session
+        $_SESSION['id_verCasa'] = $casa -> casa_id;
+        //carga el modelo en la vista 
+        $this -> view -> casa = $casa;
+        //cargar el mensaje en la vista 
+        $this -> view -> mensaje ="";
+        //carga la vista de detalle 
+        $this -> view ->render('admin/detalle');
+    }
+
+    
+    function actualizarCasa(){
+        //crear una sesion para seguridad de datos
+        session_start();
+        //utilizamos los valores del formulario y la sesion
+        $casa_id = $_SESSION['id_verCasa'];
+        $nombre = $_POST['txtnombre_casa'];
+        $calle_num = $_POST['txtcalle_num'];
+        $colonia = $_POST['txtcolonia'];
+        $ciudad = $_POST['txtciudad'];
+        $estado = $_POST['txtestado'];
+        $cp = $_POST['txtcp'];
+        $imagen = $_POST['img_image'];
+        $precio = $_POST['txtprecio'];
+        $recamaras = $_POST['txtrec'];
+        $baños = $_POST['txtbaños'];
+        //destruir sesion
+        unset($_SESSION['id_verCasa']);
+
+        if($this -> model -> update(['casa_id' => $casa_id, 
+                                    'nombre' => $nombre, 
+                                    'calle_num' => $calle_num,
+                                    'colonia' => $colonia,
+                                    'ciudad' => $ciudad,
+                                    'estado' => $estado,
+                                    'cp' => $cp,
+                                    'imagen' => $imagen,
+                                    'precio' => $precio,
+                                    'recamaras' => $recamaras,
+                                    'banos' => $baños])){
+            //actualliza catalogo
+            $casa = new Casa();
+            $casa-> casa_id = $casa_id;
+            $casa -> nombre = $nombre;
+            $casa -> calle_num = $calle_num;
+            $casa -> colonia = $colonia;
+            $casa -> ciudad = $ciudad;
+            $casa -> estado = $estado;
+            $casa -> cp = $cp;
+            $casa -> imagen = $imagen;
+            $casa -> precio = $precio;
+            $casa -> recamaras = $recamaras;
+            $casa -> baños = $baños;
+            $this -> view -> casa = $casa;
+            $this -> view -> mensaje = "Curso actualizado correctamente";
+
+        }else{
+            //error al actualizar catalogo
+            $this -> view -> mensaje = "Curso no actualizado ";
+        }
+        $this -> view -> render('admin/detalle');
+    }
+
+    function eliminarCasa($param = null){
+        $casa_id = $param[0];
+        if($this -> model -> delete($casa_id)){
+            $this -> view -> mensaje = "Casa eliminado correctamente";
+        }else{
+            $this -> view -> mensaje = "Casa no eliminada ";
+        }
+        $this ->render('admin/index');
+    }
+
+   
 }
 ?>
--- views/main/index.php
+++ views/main/index.php
@@ -1,4 +1,5 @@
 <?php
+//1234qwer# contraseña correo
   session_start();
 
   require 'database.php';
@@ -8,7 +9,7 @@
     $records->bindParam(':id', $_SESSION['id_usuario']);
     $records->execute();
     $results = $records->fetch(PDO::FETCH_ASSOC);
-    echo($_SESSION['id_usuario']);
+
 
     $user = null;
 
@@ -23,32 +24,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Multicasa</title>
-    <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/bootstrap.min.css"><?php // echo constant es para usar url absolutas ?>
-    <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/estilos.css"><?php // echo constant es para usar url absolutas ?>
-    <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/default.css"><?php // echo constant es para usar url absolutas ?>
+    <title>Document</title>
 </head>
 <body>
     <?php require 'views/header.php'; ?>
         <div id="main">
-            <div id= "slider">
+          <center>
+            <div class= "slider-container">
                <ul class="slider">
-                <li><img src="<?php echo constant('URL'); ?>public/img/casa1.png"></li>
-                <li><img src="<?php echo constant('URL'); ?>public/img/casa2.png"></li>
-                <li><img src="<?php echo constant('URL'); ?>public/img/casa3.png"></li>
-                <li><img src="<?php echo constant('URL'); ?>public/img/casa4.png"></li>
+                <li id="slide1"><img src="<?php echo constant('URL'); ?>public/img/casa1.png"></li>
+                <li id="slide2"><img src="<?php echo constant('URL'); ?>public/img/casa2.png"></li>
+                <li id="slide3"><img src="<?php echo constant('URL'); ?>public/img/casa3.png"></li>
+                <li id="slide4"><img src="<?php echo constant('URL'); ?>public/img/casa4.png"></li>
+                </ul>
+
+                <ul class ="slider-menu">
+                  <li>
+                    <a href="#slide1">1</a>
+                  </li>
+                  <li>
+                    <a href="#slide2">2</a>
+                  </li>
+                  <li>
+                    <a href="#slide3">3</a>
+                  </li>
+                  <li>
+                    <a href="#slide4">4</a>
+                  </li>
                 </ul>
             </div>
-
+            </center>
             <div id= "texto">
                 <h1>bienvenidos a multicasa</h1>
             </div>
-<<<<<<< HEAD
 
             <div id="login" >
                <?php if(!empty($user)): ?>
-      <br> Welcome. <?= $user['Nombre']; ?>
-      <br>You are Successfully Logged In
+      <br> Bienvenido. <?= $user['Nombre']; ?>
+      <br> A Iniciado sesión de manera satisfactoria
       <a href="<?php echo constant('URL'); ?>logout">
         Logout
       </a>
@@ -60,50 +73,6 @@
     <?php endif; ?>
         
         </div>
-=======
-            <div>
-                <table width="100%" class="table">
-                    <thead>
-                        <tr>
-                            <th>CASA ID</th>
-                            <th>NOMBRE</th>
-                            <th>CALLE Y NUMERO</th>
-                            <th>COLONIA</th>
-                            <th>CP</th>
-                            <th>CIUDAD</th>
-                            <th>CP</th>
-                            <th>PRECIO</th>
-                            <th>RECAMARAS</th>
-                            <th>BAÑOS</th>
-
-                        </tr>   
-                    </thead>
-                    <tbody>
-                        <?php //trae los objetos de catalogo y los muestra en una tabla 
-                        include_once 'models/casa.php';
-                        foreach($this -> casas as $row){
-                            $casa = new Casa();
-                            $casa = $row;
-                            
-                    ?>
-                    <tr>
-                        <td><?php echo $casa -> casa_id;?></td>
-                        <td><?php echo $casa -> nombre;?></td>
-                        <td><?php echo $casa -> calle_num;?></td>
-                        <td><?php echo $casa -> colonia;?></td>
-                        <td><?php echo $casa -> ciudad;?></td>
-                        <td><?php echo $casa -> cp;?></td>
-                        <td><?php echo $casa -> precio;?></td>
-                        <td><?php echo $casa -> recamaras;?></td>
-                        <td><?php echo $casa -> baños;?></td>
-                        <td><a class="btn-link" href="<?php echo constant('URL') . 'consulta/verCatalogo/' . $casa -> casa_id;?>">Editar</a></td>
-                        <td><a href="<?php echo constant('URL') . 'consulta/eliminarCatalogo/' . $casa ->casa_id;?>">Eliminar</a></td>
-                    </tr>
-                    <?php } ?>
-                    </tbody>
-                </table>
-            </div>
->>>>>>> e5d59b1e01a4da243719926ed360928d7a525eea
             <?php require 'views/menu.php'; ?>
         </div>
     <?php require 'views/footer.php'; ?>
